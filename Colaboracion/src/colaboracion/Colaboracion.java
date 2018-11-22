0da9ff14ff9bf3ad3c361c1c13d3440dcb15abe2
apinodominguez <apinodominguez@nautilus3.danielcastelao.org>
2018-11-22 09:06:56 +0100

    Ejercicio Colaboracion. Primer codigo

--- Colaboracion/src/colaboracion/Colaboracion.java
+++ Colaboracion/src/colaboracion/Colaboracion.java
@@ -1,5 +1,7 @@
 package colaboracion;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author apinodominguez
@@ -8,7 +10,10 @@ package colaboracion;
 public class Colaboracion {
 
     public static void main(String[] args) {
-        
+    int x = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero: "));
+    int y = Integer.parseInt(JOptionPane.showInputDialog("Introduce el segundo numero: "));
+    
+    JOptionPane.showMessageDialog(null, "La suma de ambos numeros es: " + (x + y));
     }
     
 }
